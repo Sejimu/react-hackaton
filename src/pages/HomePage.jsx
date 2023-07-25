77bb65712b06b5e2f3d19309dff858fed79a3dfe
asnblagowe <blagomukha@gmail.com>
2023-07-25 15:33:19 +0300

    pagination

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProductsList from "../components/ProductsList";
+import { Box, Pagination } from "@mui/material";
+import { LIMIT } from "../utils/consts";
+import { useSearchParams } from "react-router-dom";
+import { useProductContext } from "../contexts/ProductContext";
 
 function HomePage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { getProducts, pageTotalCount, page, setPage } = useProductContext();
+
+  useEffect(() => {
+    getProducts();
+  }, [searchParams]);
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries([...searchParams]);
+    setSearchParams({
+      ...currentParams,
+      _page: page,
+      _limit: LIMIT,
+    });
+  }, [page]);
   return (
     <div>
       <ProductsList />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          backgroundColor: "#001C30",
+          color: "#dafffb",
+        }}
+      >
+        <Pagination
+          count={pageTotalCount}
+          page={page}
+          onChange={(_, val) => setPage(val)}
+          color="primary"
+        />
+      </Box>
     </div>
   );
 }
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,8 +1,10 @@
 export const ACTIONS = {
   products: "products",
   product: "product",
+  pageTotalCount: "pageTotalCount",
 };
 
 export const API = "http://localhost:8000/products";
 
 export const ADMINS = ["krasavachic@gmail.com"];
+export const LIMIT = 8;
