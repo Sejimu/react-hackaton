711941e287c48b353dd25d2e4a2f28dd38fc8646
l1z1ng3r <krasavachic@gmail.com~>
2023-07-25 16:14:57 +0600

    auth done

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDg4fC1Xs-V4GoG1ycSm8F1yiSZ9PgyJVs",
-  authDomain: "hackaton-24-07-2023.firebaseapp.com",
-  projectId: "hackaton-24-07-2023",
-  storageBucket: "hackaton-24-07-2023.appspot.com",
-  messagingSenderId: "843633608612",
-  appId: "1:843633608612:web:e4f984d0e553db444fe331",
+  apiKey: "AIzaSyCHHKvqpiUYfrhZmm1tI8pqiq5I3K6pQ-U",
+  authDomain: "hackaton3-db4ed.firebaseapp.com",
+  projectId: "hackaton3-db4ed",
+  storageBucket: "hackaton3-db4ed.appspot.com",
+  messagingSenderId: "155400840442",
+  appId: "1:155400840442:web:73947d00c54e690f1fd543",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
